cc81f7cd46062c31d10405d7ca73515bf1a6d1d2
Collin Kovacs <44042857+cjk5642@users.noreply.github.com>
2023-08-16 14:30:14 +0000

    data commit

--- risp_metric.py
+++ risp_metric.py
@@ -1,10 +1,24 @@
 from pybaseball import statcast
-from pybaseball import chadwick_register
-from pybaseball import  playerid_lookup
-from pybaseball import  statcast_pitcher
-
+from pybaseball import cache
 import pandas as pd
 import os
 
+cache.enable()
+cache.config.cache_type = "parquet"
+cache.config.save()
 
-players = pd.read_csv("datasources/players.csv")
+#players = pd.read_csv("datasources/players.csv")
+years = range(2007, 2023)
+chunks = [('01-01', '05-18'), ('05-18', '06-18'), ('06-18', '07-18'), ('07-18', '08-18'), ('08-18', '12-31')]
+for year in years:
+    for first, second in chunks:
+        file_name = f"{year}-{first}:{second}"
+        print(f"Collecting data for the ranges: {file_name}")
+        first_date = f"{year}-{first}"
+        second_date = f"{year}-{second}"
+        file_path = os.path.join("pbp_data", f"{file_name}.parquet")
+        if not os.path.exists(file_path):
+            data = statcast(first_date, second_date)
+            data.to_parquet(file_path, index=False)
+        else:
+            print("File already exists.\n")
